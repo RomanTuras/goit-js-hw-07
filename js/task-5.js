2ff4d0932885a790b8148a097c6daf2648f5409c
Roman <salden.com@gmail.com>
2024-09-02 16:05:25 +0300

    Add task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,15 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector('.change-color');
+const body = document.querySelector('body');
+
+const changeColor = () => {
+    let color = getRandomHexColor();
+    body.style.backgroundColor = color;
+};
+
+changeColorBtn.addEventListener('click', changeColor);

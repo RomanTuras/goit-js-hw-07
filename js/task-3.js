6c262db8d7566532eb831513a25889f1b23e5893
Roman <salden.com@gmail.com>
2024-09-02 15:21:59 +0300

    Formatting code

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,9 @@
-const input = document.querySelector("#name-input");
-let output = document.querySelector("#name-output");
+const input = document.querySelector('#name-input');
+let output = document.querySelector('#name-output');
 
 const onChangeInput = () => {
-  let text = input.value.trim();
-  output.textContent = text.length > 0 ? text : "Anonymous";
-}
+    let text = input.value.trim();
+    output.textContent = text.length > 0 ? text : 'Anonymous';
+};
 
-input.addEventListener("input", onChangeInput);
+input.addEventListener('input', onChangeInput);

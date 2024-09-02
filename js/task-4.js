ca94e7f86aedc0d2503d939d30c536f454afd459
Roman <salden.com@gmail.com>
2024-09-02 15:57:49 +0300

    Add task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form');
+const onSubmit = event => {
+    event.preventDefault();
+    let email = form.elements.email.value;
+    let password = form.elements.password.value;
 
+    if (email.length == 0 || password.length == 0) {
+        alert('All form fields must be filled in');
+    } else {
+        let credentials = {
+            email: email.trim(),
+            password: password.trim(),
+        };
+        console.log(credentials);
+        form.reset();
+    }
+};
+
+form.addEventListener('submit', onSubmit);
